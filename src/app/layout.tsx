a32a1f4ce7319af05bbf9738e19b1c5fcb08576f
Dhruvkoshta <Dhruvkoshta04@gmail.com>
2025-03-21 22:03:24 +0530

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "next-themes"
+import { PostHogProvider } from "./_providers/providers"
 // import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -21,7 +22,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          <PostHogProvider>
           {children}
+          </PostHogProvider>
         </ThemeProvider>
       </body>
     </html>
