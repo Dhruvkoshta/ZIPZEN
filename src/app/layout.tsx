e528459458e5b00ec357c23b4f19266df92db5ba
Dhruvkoshta <Dhruvkoshta04@gmail.com>
2025-03-22 13:07:58 +0530

    metadata changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,35 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "next-themes"
-import { PostHogProvider } from "./_providers/providers"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import { PostHogProvider } from "./_providers/providers";
 // import { ThemeProvider } from "@/components/theme-provider"
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Google Drive Clone",
-  description: "A Google Drive clone UI with dark mode and collapsible sidebar",
-}
+	title: "ZIP ZEN",
+	description: "A clound data storage platform made with NEXTJS by DK",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          <PostHogProvider>
-          {children}
-          </PostHogProvider>
-        </ThemeProvider>
-      </body>
-    </html>
-
-  )
+	return (
+		<html lang='en' suppressHydrationWarning>
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='dark'
+					enableSystem
+					disableTransitionOnChange
+				>
+					<PostHogProvider>{children}</PostHogProvider>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
-
